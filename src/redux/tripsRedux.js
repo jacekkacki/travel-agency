7bb7056745e26608c6c83389fcf1d740dd81c749
Jacek Kącki <jkacki@gmail.com>
2019-12-23 19:02:09 +0100

    change tripsRedux.js

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -12,8 +12,15 @@ export const getFilteredTrips = ({trips, filters}) => {
   }
 
   // TODO - filter by duration
+  if(filters.duration) { output = output.filter(trip => trip.days >= filters.duration.from
+     && trip.days <= filters.duration.to );
+  }
 
   // TODO - filter by tags
+  if(filters.tags) { output = output.filter(trip =>
+    filters.tags.every(tag =>  trip.tags.indexOf(tag) > -1)
+  );
+  }
 
   // TODO - sort by cost descending (most expensive goes first)
 
