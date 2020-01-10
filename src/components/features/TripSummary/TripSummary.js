3201b7d277baa127b96e356c47516084e3cc6d79
Jacek Kącki <jkacki@gmail.com>
2020-01-10 20:22:43 +0100

    add tests components Hero and TripSummary

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -14,11 +14,12 @@ const TripSummary = ({id, image, name, cost, days, tags}) => (
           <span>{days} days</span>
           <span>from {cost}</span>
         </div>
+        {tags &&
         <div className={styles.tags}>
           {tags.map(tag => (
             <span className={styles.tag} key={tag.toString()}>{tag}</span>
           ))}
-        </div>
+        </div>}
       </article>
     </Link>
   </Col>
---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+
+  it('should render correct link', () => {
+    const expectedLink = 'abc';
+    const component = shallow(<TripSummary id = {expectedLink} />);
+    expect(component.find('Link').prop('to')).toEqual(`/trip/${expectedLink}`);
+  });
+
+
+  it('Does image have correct properties src and alt', () => {
+    const expectedSrc = 'image.jpg';
+    const expectedAlt = 'Alternative text';
+    const component = shallow(<TripSummary image = {expectedSrc} name = {expectedAlt} />);
+    expect(component.find('img').prop('src')).toEqual(expectedSrc);
+    expect(component.find('img').prop('alt')).toEqual(expectedAlt);
+  });
+
+  it('should render correct props name, days, cost', () => {
+    const expectedName = 'Trip';
+    const expectedDays = 14;
+    const expectedCost = '$1000';
+    const component = shallow(
+      <TripSummary 
+        name={expectedName}
+        cost = {expectedCost}
+        days = {expectedDays}
+      />);
+    expect(component.find('.title').text()).toEqual(expectedName);
+    expect(component.find('.details span').at(0).text()).toEqual(`${expectedDays} days`);
+    expect(component.find('.details span').at(1).text()).toEqual(`from ${expectedCost}`);
+    
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<TripSummary />)).toThrow();
+  });
+
+  it('should render correct order tags array', () => {
+    const expectedTags = ['one', 'two', 'three'];
+    const component = shallow(
+      <TripSummary tags={expectedTags} />);
+    expect(component.find('.tags span').at(0).text()).toEqual(expectedTags[0]);
+    expect(component.find('.tags span').at(1).text()).toEqual(expectedTags[1]);
+    expect(component.find('.tags span').at(2).text()).toEqual(expectedTags[2]);
+  });
+
+  it('should render when props tags is correct', () => {
+    const expectedTags = [];
+    const component = shallow(<TripSummary tags={expectedTags} />);
+    expect(component.find('.tags')).toBeTruthy();
+  });
+
+});
