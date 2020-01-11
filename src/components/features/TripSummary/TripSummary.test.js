669e12fa56e7e78fa3657f703f09a87886839ef0
Jacek Kącki <jkacki@gmail.com>
2020-01-11 21:01:20 +0100

    change tests components TripSummary.test.js

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -5,35 +5,54 @@ import TripSummary from './TripSummary';
 describe('Component TripSummary', () => {
 
   it('should render correct link', () => {
-    const expectedLink = 'abc';
+    const expectedSrc = 'image.jpg';
     const expectedName = 'Trip';
+    const expectedId = 'abc';
+    const expectedDays = 14;
+    const expectedCost = '$1000';
     const component = shallow(
-      <TripSummary id = {expectedLink} name = {expectedName}/>);
-    expect(component.find('Link').prop('to')).toEqual(`/trip/${expectedLink}`);
+      <TripSummary
+        id = {expectedId}
+        name = {expectedName}
+        image = {expectedSrc}
+        cost = {expectedCost}
+        days = {expectedDays}
+      />);
+    expect(component.find('Link').prop('to')).toEqual(`/trip/${expectedId}`);
   });
 
 
   it('Does image have correct properties src and alt', () => {
     const expectedSrc = 'image.jpg';
-    const expectedAlt = 'Alternative text';
+    const expectedName = 'Trip';
     const expectedId = 'abc';
+    const expectedDays = 14;
+    const expectedCost = '$1000';
     const component = shallow(
-      <TripSummary image = {expectedSrc} name = {expectedAlt} id = {expectedId}/>);
+      <TripSummary
+        image = {expectedSrc}
+        name = {expectedName}
+        id = {expectedId}
+        days = {expectedDays}
+        cost = {expectedCost}
+      />);
     expect(component.find('img').prop('src')).toEqual(expectedSrc);
-    expect(component.find('img').prop('alt')).toEqual(expectedAlt);
+    expect(component.find('img').prop('alt')).toEqual(expectedName);
   });
 
   it('should render correct props name, days, cost', () => {
+    const expectedSrc = 'image.jpg';
     const expectedName = 'Trip';
+    const expectedId = 'abc';
     const expectedDays = 14;
     const expectedCost = '$1000';
-    const expectedId = 'abc';
     const component = shallow(
       <TripSummary 
+        image = {expectedSrc}
         name = {expectedName}
-        cost = {expectedCost}
-        days = {expectedDays}
         id = {expectedId}
+        days = {expectedDays}
+        cost = {expectedCost}
       />);
     expect(component.find('.title').text()).toEqual(expectedName);
     expect(component.find('.details span').at(0).text()).toEqual(`${expectedDays} days`);
@@ -46,10 +65,20 @@ describe('Component TripSummary', () => {
 
   it('should render correct order tags array', () => {
     const expectedTags = ['one', 'two', 'three'];
+    const expectedSrc = 'image.jpg';
     const expectedName = 'Trip';
     const expectedId = 'abc';
+    const expectedDays = 14;
+    const expectedCost = '$1000';
     const component = shallow(
-      <TripSummary tags={expectedTags} name={expectedName} id={expectedId}/>);
+      <TripSummary 
+        tags={expectedTags}
+        image = {expectedSrc}
+        name = {expectedName}
+        id = {expectedId}
+        days = {expectedDays}
+        cost = {expectedCost}
+      />);
     expect(component.find('.tags span').at(0).text()).toEqual(expectedTags[0]);
     expect(component.find('.tags span').at(1).text()).toEqual(expectedTags[1]);
     expect(component.find('.tags span').at(2).text()).toEqual(expectedTags[2]);
@@ -57,10 +86,20 @@ describe('Component TripSummary', () => {
 
   it('should render when props tags is correct', () => {
     const expectedTags = [];
+    const expectedSrc = 'image.jpg';
     const expectedName = 'Trip';
     const expectedId = 'abc';
+    const expectedDays = 14;
+    const expectedCost = '$1000';
     const component = shallow(
-      <TripSummary tags={expectedTags} name={expectedName} id={expectedId} />);
+      <TripSummary
+        tags={expectedTags}
+        image = {expectedSrc}
+        name = {expectedName}
+        id = {expectedId}
+        days = {expectedDays}
+        cost = {expectedCost}
+      />);
     expect(component.find('.tags')).toBeTruthy();
   });
 
